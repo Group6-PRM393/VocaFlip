51dbd12c2acae3ee5a58e5dcf59d8b7a572051d6
dotuananh <dotuananh1625@gmail.com>
2026-03-22 21:05:40 +0700

    [FE] - update top score

--- vocaflip-backend/src/main/java/com/vocaflipbackend/dto/response/FlipMatchGameSummaryResponse.java
+++ vocaflip-backend/src/main/java/com/vocaflipbackend/dto/response/FlipMatchGameSummaryResponse.java
@@ -3,10 +3,13 @@ package com.vocaflipbackend.dto.response;
 import lombok.Builder;
 import lombok.Data;
 
+import java.util.List;
+
 @Data
 @Builder
 public class FlipMatchGameSummaryResponse {
     private long totalScore;
     private long totalGames;
     private int bestScore;
+    private List<Integer> top3Scores;
 }
